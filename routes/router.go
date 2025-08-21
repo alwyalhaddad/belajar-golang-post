4b2016fa612467775985f30473118e99d7087090
alwi zainal abidin <alwizaenalabidin@gmail.com>
2025-08-21 17:48:30 +0700

    update

--- routes/router.go
+++ routes/router.go
@@ -8,16 +8,31 @@ import (
 )
 
 func MainRoutes(router *gin.Engine, db *gorm.DB) {
+	authMiddleware := middleware.AuthMiddleware(db)
+
 	mainGroup := router.Group("")
 	{
 		mainGroup.POST("/register", controllers.Register(db))
 		mainGroup.POST("/login", controllers.Login(db))
 		mainGroup.POST("/logout", controllers.Logout(db))
-		mainGroup.POST("/products", controllers.CreateProduct(db))
-		mainGroup.GET("/products", controllers.GetAllProduct(db))
-		mainGroup.GET("/products/:id", controllers.GetProductById(db))
-		mainGroup.PUT("/products/:id")
 		mainGroup.POST("/forgotpassword", controllers.ForgotPassword(db))
-		mainGroup.POST("/changepassword", middleware.AuthMiddleware(db), controllers.ChangePassword(db))
+		mainGroup.POST("/changepassword", controllers.ChangePassword(db), authMiddleware)
+	}
+
+	productGroup := router.Group("")
+	{
+		productGroup.POST("/products", controllers.CreateProduct(db))
+		productGroup.GET("/products", controllers.GetAllProduct(db))
+		productGroup.GET("/products/:id", controllers.GetProductById(db))
+		productGroup.PUT("/products/:id")
+	}
+
+	cartGroup := router.Group("cart")
+	cartGroup.Use(authMiddleware)
+	{
+		cartGroup.POST("/items", controllers.AddItemToCart(db))
+		cartGroup.GET("/items", controllers.GetCart(db))
+		cartGroup.PUT("/items/:id", controllers.UpdateCartItemQuantity(db))
+		cartGroup.DELETE("items/:id", controllers.RemoveCartItem(db))
 	}
 }
